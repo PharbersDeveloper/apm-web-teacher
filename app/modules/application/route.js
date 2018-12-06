f84bce76944b09362b4fce1a8e9a690ced04d1a8
Frank <wsgoodman@163.com>
2018-12-06 15:37:56 +0800

    提交 application中的注释。

--- app/modules/application/route.js
+++ app/modules/application/route.js
@@ -7,11 +7,9 @@ export default Route.extend({
 	beforeModel() {
 		moment.locale('zh-cn');
 		// let token = this.get('cookies').read('token'),
-		// 	loginController = this.controllerFor('index');
+		// loginController = this.controllerFor('index');
 
-		this.get('cookies').clear('token', { path: '/' });
-		localStorage.clear();
-		this.transitionTo('index');
+
 		// if (!token) {
 		// 	if (transition.targetName !== 'index') {
 		// 		loginController.set('previousTransition', transition);
@@ -21,6 +19,12 @@ export default Route.extend({
 		// } else if (transition.targetName === 'index') {
 		// 	this.transitionTo('output');
 		// }
+		/**
+		 * 每次刷新清除cookies
+		 */
+		this.get('cookies').clear('token', { path: '/' });
+		localStorage.clear();
+		this.transitionTo('index');
 	},
 	acrivate() {
 		this.controllerFor('application').set('userName', localStorage.getItem('userName'));
