f828076bad68e48edcde15f0e8c59382363ba5fc
Faiz <891366402@qq.com>
2018-12-06 17:19:48 +0800

    更新Adapter

--- app/modules/application/adapter.js
+++ app/modules/application/adapter.js
@@ -1,4 +1,20 @@
 import PharbersAdapter from 'pharbers-emberbasis-library/adapters/phadapter';
 
 export default PharbersAdapter.extend({
+	init() {
+		let token = document.cookie.split(';').find(elem => elem.indexOf('token-t') > -1);
+
+		if (typeof token === 'undefined') {
+			token = 0;
+		} else {
+			token = token.split('=')[1];
+		}
+
+		this.set('headers', {
+			'dataType': 'json',
+			'contentType': 'application/json',
+			'Content-Type': 'application/json',
+			'Authorization': `bearer ${token}`
+		});
+	}
 });
